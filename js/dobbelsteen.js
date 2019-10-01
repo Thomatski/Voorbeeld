b5ad3c6a274adbacd2cc05138592bf5d0c76dbb0
Thomatski <55575536+Thomatski@users.noreply.github.com>
2019-10-01 09:04:21 +0200

    dobbelsteen images added

--- js/dobbelsteen.js
+++ js/dobbelsteen.js
@@ -1,11 +1,13 @@
 var uitvoerDobbelsteen = document.getElementById("uitvoerDobbelsteen");
 var btnDobbelsteen = document.getElementById("btnDobbelsteen");
 
+
 btnDobbelsteen.addEventListener("click", function(){
-    let getal = getRandomint(1, 6);
-    console.log(Math.round(Math.random()* 10));    
+    let getal = getRandomInt(1, 6);
+    
+    uitvoerDobbelsteen.innerHTML = getal;
 });
 
 function getRandomInt(min, max) {
-    return Math.round(Math.random()* (max-min+1)+ min);
+    return Math.round(Math.random()* (max - min)+ min);
 };
